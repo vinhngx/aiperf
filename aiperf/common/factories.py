eee47e754cc79e0a429563d80be6ff4ef1aec33f
Anthony Casagrande <acasagrande@nvidia.com>
2025-07-24 11:02:01 -0700

    chore: sort factories by name in common factories file (#123)

--- aiperf/common/factories.py
+++ aiperf/common/factories.py
@@ -1,28 +1,35 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
-import logging
 from collections.abc import Callable
-from typing import Any, Generic, TypeVar
+from typing import Any, Generic
 
+from aiperf.common.aiperf_logger import AIPerfLogger
 from aiperf.common.enums import (
-    CaseInsensitiveStrEnum,
+    CommClientType,
+    CommunicationBackend,
+    ComposerType,
+    CustomDatasetType,
+    DataExporterType,
+    PostProcessorType,
     ServiceType,
     StreamingPostProcessorType,
+    ZMQProxyType,
 )
-from aiperf.common.exceptions import FactoryCreationError
+from aiperf.common.enums.endpoints_enums import EndpointType
+from aiperf.common.enums.service_enums import ServiceRunType
+from aiperf.common.exceptions import FactoryCreationError, InvalidOperationError
+from aiperf.common.types import (
+    ClassEnumT,
+    ClassProtocolT,
+    ServiceProtocolT,
+    ServiceTypeT,
+)
 
-ClassEnumT = TypeVar("ClassEnumT", bound=CaseInsensitiveStrEnum)
-ClassProtocolT = TypeVar("ClassProtocolT", bound=Any)
 
-################################################################################
-# Generic Base Factory Mixin
-################################################################################
+class AIPerfFactory(Generic[ClassEnumT, ClassProtocolT]):
+    """Defines a custom factory for AIPerf components.
 
-
-class FactoryMixin(Generic[ClassEnumT, ClassProtocolT]):
-    """Defines a mixin for all factories, which supports registering and creating instances of classes.
-
-    This mixin is used to create a factory for a given class type and protocol.
+    This class is used to create a factory for a given class type and protocol.
 
     Example:
     ```python
@@ -65,15 +72,14 @@ class FactoryMixin(Generic[ClassEnumT, ClassProtocolT]):
     ```
     """
 
-    logger = logging.getLogger(__name__)
-
+    _logger: AIPerfLogger
     _registry: dict[ClassEnumT | str, type[ClassProtocolT]]
     _override_priorities: dict[ClassEnumT | str, int]
 
     def __init_subclass__(cls) -> None:
         cls._registry = {}
         cls._override_priorities = {}
-        cls.logger = logging.getLogger(cls.__name__)
+        cls._logger = AIPerfLogger(cls.__name__)
         super().__init_subclass__()
 
     @classmethod
@@ -110,34 +116,21 @@ class FactoryMixin(Generic[ClassEnumT, ClassProtocolT]):
         def decorator(class_cls: type[ClassProtocolT]) -> type[ClassProtocolT]:
             existing_priority = cls._override_priorities.get(class_type, -1)
             if class_type in cls._registry and existing_priority >= override_priority:
-                cls.logger.warning(
-                    "%r class %s already registered with same or higher priority "
-                    "(%s). The new registration of class %s with priority "
-                    "%s will be ignored.",
-                    class_type,
-                    cls._registry[class_type].__name__,
-                    existing_priority,
-                    class_cls.__name__,
-                    override_priority,
+                cls._logger.warning(
+                    f"{class_type!r} class {cls._registry[class_type].__name__} already registered with same or higher priority "
+                    f"({existing_priority}). The new registration of class {class_cls.__name__} with priority "
+                    f"{override_priority} will be ignored.",
                 )
                 return class_cls
 
             if class_type not in cls._registry:
-                cls.logger.debug(
-                    "%r class %s registered with priority %s.",
-                    class_type,
-                    class_cls.__name__,
-                    override_priority,
+                cls._logger.debug(
+                    lambda: f"{class_type!r} class {class_cls.__name__} registered with priority {override_priority}.",
                 )
             else:
-                cls.logger.warning(
-                    "%r class %s with priority %s overrides "
-                    "already registered class %s with lower priority (%s).",
-                    class_type,
-                    class_cls.__name__,
-                    override_priority,
-                    cls._registry[class_type].__name__,
-                    existing_priority,
+                cls._logger.warning(
+                    f"{class_type!r} class {class_cls.__name__} with priority {override_priority} overrides "
+                    f"already registered class {cls._registry[class_type].__name__} with lower priority ({existing_priority}).",
                 )
             cls._registry[class_type] = class_cls
             cls._override_priorities[class_type] = override_priority
@@ -164,12 +157,14 @@ class FactoryMixin(Generic[ClassEnumT, ClassProtocolT]):
             FactoryCreationError: If the class type is not registered or there is an error creating the instance
         """
         if class_type not in cls._registry:
-            raise FactoryCreationError(f"No implementation found for {class_type!r}.")
+            raise FactoryCreationError(
+                f"No implementation registered for {class_type!r} in {cls.__name__}."
+            )
         try:
             return cls._registry[class_type](**kwargs)
         except Exception as e:
             raise FactoryCreationError(
-                f"Error creating {class_type!r} instance: {e}"
+                f"Error creating {class_type!r} instance for {cls.__name__}: {e}"
             ) from e
 
     @classmethod
@@ -213,113 +208,119 @@ class FactoryMixin(Generic[ClassEnumT, ClassProtocolT]):
         return [(cls, class_type) for class_type, cls in cls._registry.items()]
 
 
-################################################################################
-# Built-in Factories
-################################################################################
-
-
-class ServiceFactory(FactoryMixin[ServiceType, "BaseService"]):
-    """Factory for registering and creating BaseService instances based on the specified service type.
-
-    Example:
-    ```python
-        # Register a new service type
-        @ServiceFactory.register(ServiceType.DATASET_MANAGER)
-        class DatasetManager(BaseService):
-            pass
-
-        # Create a new service instance in a separate process
-        service_class = ServiceFactory.get_class_from_type(service_type)
-
-        process = Process(
-            target=bootstrap_and_run_service,
-            name=f"{service_type}_process",
-            args=(service_class, self.config),
-            daemon=False,
-        )
-    ```
+class CommunicationClientFactory(
+    AIPerfFactory[CommClientType, "CommunicationClientProtocol"]
+):
+    """Factory for registering and creating CommunicationClientProtocol instances based on the specified communication client type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
     """
 
 
-class DataExporterFactory(FactoryMixin["DataExporterType", "DataExporterProtocol"]):
-    """Factory for registering and creating DataExporterInterface instances.
-
-    Example:
-    ```python
-        # Iterate over all registered data exporter types
-        for exporter_class in DataExporterFactory.get_all_classes():
-            exporter = exporter_class(endpoint_config)
-
-            exporter.export()
-    ```
+class CommunicationClientProtocolFactory(
+    AIPerfFactory[CommClientType, "CommunicationClientProtocol"]
+):
+    """Factory for registering and creating CommunicationClientProtocol instances based on the specified communication client type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
     """
 
 
-class PostProcessorFactory(FactoryMixin["PostProcessorType", "PostProcessorProtocol"]):
-    """Factory for registering and creating PostProcessor instances based on the specified post-processor type.
-
-    Example:
-    ```python
-        # Register a new post-processor type
-        @PostProcessorFactory.register(PostProcessorType.METRIC_SUMMARY)
-        class MetricSummary:
-            pass
-
-        # Create a new post-processor instance
-        post_processor = PostProcessorFactory.create_instance(
-            PostProcessorType.METRIC_SUMMARY,
-        )
+class CommunicationFactory(
+    AIPerfFactory[CommunicationBackend, "CommunicationProtocol"]
+):
+    """Factory for registering and creating CommunicationProtocol instances based on the specified communication backend.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
     """
 
 
-class ComposerFactory(FactoryMixin["ComposerType", "BaseDatasetComposer"]):
-    """Factory for registering and creating BaseDatasetComposer instances
-    based on the specified composer type.
-
-    Example:
-    ```python
-        # Register a new composer type
-        @ComposerFactory.register(ComposerType.SYNTHETIC)
-        class SyntheticDatasetComposer(BaseDatasetComposer):
-            pass
-
-        # Create a new composer instance
-        composer = ComposerFactory.create_instance(
-            ComposerType.SYNTHETIC,
-            config=InputConfig(
-                conversation=ConversationConfig(num=10),
-                prompt=PromptConfig(batch_size=10),
-            )
-        )
-    ```
+class ComposerFactory(AIPerfFactory[ComposerType, "BaseDatasetComposer"]):
+    """Factory for registering and creating BaseDatasetComposer instances based on the specified composer type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
     """
 
 
 class CustomDatasetFactory(
-    FactoryMixin["CustomDatasetType", "CustomDatasetLoaderProtocol"]
+    AIPerfFactory[CustomDatasetType, "CustomDatasetLoaderProtocol"]
 ):
+    """Factory for registering and creating CustomDatasetLoaderProtocol instances based on the specified custom dataset type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
     """
-    Factory for registering and creating CustomDatasetLoader instances
-    based on the specified custom dataset type.
 
-    Example:
-    ```python
-        # Register a new custom dataset type
-        @CustomDatasetFactory.register(CustomDatasetType.MOONCAKE_TRACE)
-        class MooncakeTraceDatasetLoader(CustomDatasetLoader):
-            pass
 
-        # Create a new custom dataset loader instance
-        custom_dataset_loader = CustomDatasetFactory.create_instance(
-            CustomDatasetType.MOONCAKE_TRACE, **kwargs
+class DataExporterFactory(AIPerfFactory[DataExporterType, "DataExporterProtocol"]):
+    """Factory for registering and creating DataExporterProtocol instances based on the specified data exporter type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
+    """
+
+
+class InferenceClientFactory(AIPerfFactory[EndpointType, "InferenceClientProtocol"]):
+    """Factory for registering and creating InferenceClientProtocol instances based on the specified endpoint type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
+    """
+
+
+class PostProcessorFactory(AIPerfFactory[PostProcessorType, "PostProcessorProtocol"]):
+    """Factory for registering and creating PostProcessorProtocol instances based on the specified post processor type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
+    """
+
+
+class RequestConverterFactory(AIPerfFactory[EndpointType, "RequestConverterProtocol"]):
+    """Factory for registering and creating RequestConverterProtocol instances based on the specified request payload type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
+    """
+
+
+class ResponseExtractorFactory(
+    AIPerfFactory[EndpointType, "ResponseExtractorProtocol"]
+):
+    """Factory for registering and creating ResponseExtractorProtocol instances based on the specified response extractor type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
+    """
+
+
+class ServiceFactory(AIPerfFactory[ServiceType, "ServiceProtocol"]):
+    """Factory for registering and creating BaseService instances based on the specified service type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
+    """
+
+    @classmethod
+    def register_all(
+        cls, *class_types: ServiceTypeT, override_priority: int = 0
+    ) -> Callable[..., Any]:
+        raise InvalidOperationError(
+            "ServiceFactory.register_all is not supported. A single service can only be registered with a single type."
         )
-    ```
+
+    @classmethod
+    def register(
+        cls, class_type: ServiceTypeT, override_priority: int = 0
+    ) -> Callable[..., Any]:
+        # Override the register method to set the service_type on the class
+        original_decorator = super().register(class_type, override_priority)
+
+        def decorator(class_cls: type[ServiceProtocolT]) -> type[ServiceProtocolT]:
+            class_cls.service_type = class_type
+            original_decorator(class_cls)
+            return class_cls
+
+        return decorator
+
+
+class ServiceManagerFactory(AIPerfFactory[ServiceRunType, "ServiceManagerProtocol"]):
+    """Factory for registering and creating ServiceManagerProtocol instances based on the specified service run type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
     """
 
 
 class StreamingPostProcessorFactory(
-    FactoryMixin[StreamingPostProcessorType, "StreamingPostProcessor"]
+    AIPerfFactory[StreamingPostProcessorType, "BaseStreamingPostProcessor"]
 ):
-    """Factory for creating StreamingPostProcessor instances.
-    see: :class:`FactoryMixin` for more details.
+    """Factory for registering and creating BaseStreamingPostProcessor instances based on the specified streaming post processor type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
+    """
+
+
+class ZMQProxyFactory(AIPerfFactory[ZMQProxyType, "BaseZMQProxy"]):
+    """Factory for registering and creating BaseZMQProxy instances based on the specified ZMQ proxy type.
+    see: :class:`aiperf.common.factories.AIPerfFactory` for more details.
     """
