7ff91a798cc55c305c44d894cda5a086fb503f43
Anthony Casagrande <acasagrande@nvidia.com>
2025-10-22 14:50:36 -0700

    chore: update mkinit paths (#388)
    Signed-off-by: Anthony Casagrande <acasagrande@nvidia.com>

--- tools/generate_init_files.sh
+++ tools/generate_init_files.sh
@@ -8,7 +8,7 @@ if [ -f "${ACTIVATE_VENV}" ]; then
     source "${ACTIVATE_VENV}"
 fi
 
-mkinit --write --black --nomods --recursive aiperf
+mkinit --write --black --nomods --recursive src/aiperf
 # Ruff check and fix just the __init__.py files, because mkinit sometimes
 # doesn't sort the imports correctly, causing infinite error loops.
-find aiperf -name '__init__.py' | xargs ruff check --fix
+find src/aiperf -name '__init__.py' | xargs ruff check --fix
